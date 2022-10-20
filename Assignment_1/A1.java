e561035f6c61202aa9c306f04b504771c923747a
Vishwanath Shinde <vishwanathshinde988@gmail.com>
2022-10-20 19:03:19 +0530

    first

---
+++ Assignment_1/A1.java
@@ -0,0 +1,19 @@
+
+import java.io.*;
+
+public class A1
+{
+    public static void main(String[] args) throws IOException
+    {
+        int num, sum = 0;
+
+        BufferedReader obj = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter the number: ");
+        num = Integer.parseInt(obj.readLine());
+        for (int i=1; i<11; i++)
+        {
+            sum = num * i;
+            System.out.println(num + "*" + i + "=" + sum);
+        }
+    }
+}
