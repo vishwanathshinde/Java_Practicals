632985a42b503d3b2618eff1c99b8650d3a56b00
Vishwanath Shinde <vishwanathshinde988@gmail.com>
2022-10-20 20:31:51 +0530

    first

---
+++ Assignment_1/B2.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Employee
+{
+    public String name;
+    public double salary;
+
+    Scanner sc = new Scanner(System.in);
+    public void getdata()
+    {
+        System.out.print("Enter name: ");
+        name = sc.next();
+        System.out.print("Enter salary: ");
+        salary = sc.nextDouble();
+    }
+    public void dispay()
+    {
+        System.out.println("Employee Info: ");
+        System.out.println("Name is: "+ name);
+        System.out.println("Salary is: "+ salary);
+        System.out.println("---------------");
+    }
+}
+public class B2
+{
+    public static void main(String args[])
+    {
+        Employee e1[] = new Employee[5];
+        for(int i=0; i<e1.length; i++)
+        {
+            e1[i] = new Employee();
+            e1[i].getdata();
+        }
+        for(int i=0; i<e1.length; i++)
+        {
+            e1[i].dispay();
+        }
+    }
+}
